2d091dd858be5f64b37f3b42dcbbff4f47659998
logier <11142069+logier@user.noreply.gitee.com>
2024-02-21 16:00:51 +0800

    所有api发送

--- apps/sendpic.js
+++ apps/sendpic.js
@@ -26,22 +26,26 @@ export class example extends plugin {
   get imageConfig () { return getFunctionData('url', 'setimage', '定时发图') }
 
   
-  // 定时任务
   async 定时发图 () {
-
     if (!this.pushConfig.isAutoPush) {return false}
   
-    const image = this.imageConfig.Switch ? await getRandomImage() : await getRandomUrl(this.imageConfig.imageUrls); 
-
+    // 获取 imageUrls 数组
+    const imageUrls = this.imageConfig.imageUrls;
+  
     logger.info(`[定时发图]开始推送……`);
     for (let i = 0; i < this.pushConfig.groupList.length; i++) {
-      setTimeout(() => {
-        Bot.pickGroup(this.pushConfig.groupList[i]).sendMsg([segment.image(image)]);
-      }, 1 * 1000); 
+      // 遍历 imageUrls 数组
+      for (let j = 0; j < imageUrls.length; j++) {
+        setTimeout(() => {
+          // 使用数组的每一位发送
+          Bot.pickGroup(this.pushConfig.groupList[i]).sendMsg([segment.image(imageUrls[j])]);
+        }, j * 1000); 
+      }
     }
-
+  
     return true
   }
+  
 
   async 发图 (e) {
 
