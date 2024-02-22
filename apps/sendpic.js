3a7e3202758d5ffc8e67186b0857b7eeb68401a0
logier <11142069+logier@user.noreply.gitee.com>
2024-02-22 20:39:44 +0800

    同时推送多个图源

--- apps/sendpic.js
+++ apps/sendpic.js
@@ -1,4 +1,4 @@
-import { getFunctionData, getRandomImage,  getRandomUrl } from '../utils/getdate.js'
+import { getFunctionData, getRandomUrl, readAndParseYAMLNotasync } from '../utils/getdate.js'
 
 export class example extends plugin {
   constructor() {
@@ -24,30 +24,55 @@ export class example extends plugin {
 
   get pushConfig () { return getFunctionData('push', 'setpush', '定时发图') }
   get imageConfig () { return getFunctionData('url', 'setimage', '定时发图') }
+  get switchConfig () { return  readAndParseYAMLNotasync('../config/config.yaml') }
 
   
-  // 定时任务
-  async 定时发图 () {
+// 定时任务
+async 定时发图 () {
+  if (!this.pushConfig.isAutoPush) {return false}
 
-    if (!this.pushConfig.isAutoPush) {return false}
-  
-    const image = this.imageConfig.Switch ? await getRandomImage() : await getRandomUrl(this.imageConfig.imageUrls); 
+  logger.info(`[定时发图]开始推送……`);
 
-    logger.info(`[定时发图]开始推送……`);
+  if (this.switchConfig.sendpicrandom) {
+    // 如果Switch为true，获取随机图片并发送
+    const image = await getRandomUrl(this.imageConfig.imageUrls);
     for (let i = 0; i < this.pushConfig.groupList.length; i++) {
       setTimeout(() => {
         Bot.pickGroup(this.pushConfig.groupList[i]).sendMsg([segment.image(image)]);
-      }, 1 * 1000); 
+      }, 1 * 3000); 
     }
-
-    return true
+  } else {
+    // 如果Switch为false，遍历imageUrls数组并发送每一张图片
+    const imageUrls = this.imageConfig.imageUrls;
+    for (let i = 0; i < this.pushConfig.groupList.length; i++) {
+      for (let j = 0; j < imageUrls.length; j++) {
+        setTimeout(() => {
+          Bot.pickGroup(this.pushConfig.groupList[i]).sendMsg([segment.image(imageUrls[j])]);
+        }, j * 3000); 
+      }
+    }
   }
+
+  return true
+}
+
 
   async 发图 (e) {
 
-    const image = this.imageConfig.Switch ? await getRandomImage() : await getRandomUrl(this.imageConfig.imageUrls); 
-  
-    e.reply([segment.image(image)]);
+    if (this.switchConfig.sendpicrandom) {
+      // 如果Switch为true，获取随机图片并发送
+      const image = await getRandomUrl(this.imageConfig.imageUrls);
+          e.reply([segment.image(image)]);
+    } else {
+      // 如果Switch为false，遍历imageUrls数组并发送每一张图片
+      const imageUrls = this.imageConfig.imageUrls;
+        for (let j = 0; j < imageUrls.length; j++) {
+          setTimeout(() => {
+            e.reply([segment.image(imageUrls[j])]);
+          }, j * 3000); 
+        }
+      
+    }
 
     return true
   }
