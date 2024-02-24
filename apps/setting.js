e6ca248c1c3582aafc4d31eb9bcf671fe1815fb4
logier <11142069+logier@user.noreply.gitee.com>
2024-02-25 00:12:13 +0800

    修复bug

--- apps/setting.js
+++ apps/setting.js
@@ -4,10 +4,10 @@ import setting from "../model/setting.js";
 export class TextMsg extends plugin {
     constructor() {
         super({
-            name: '测试插件', // 插件名称
-            dsc: '这是一个基础的插件示例',  // 插件描述            
+            name: '[鸢尾花]设置', // 插件名称
+            dsc: '设置',  // 插件描述            
             event: 'message',  // 更多监听事件请参考下方的 Events
-            priority: 6,   // 插件优先度，数字越小优先度越高
+            priority: 5000,   // 插件优先度，数字越小优先度越高
             rule: [
                 {
                     reg: '^#?鸢尾花(开启|关闭)(本群)?定时发图$',   // 正则表达式,有关正则表达式请自行百度
